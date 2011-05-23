176b12784ad9d6be2342495adf0ab5f1106e2bb0
Ben Paddock <essexpads@googlemail.com>
2011-05-23 09:01:21 +0100

    Added example of intercepting default actions.

---
+++ Intents/src/com/bt/devcon/android/intents/WebSearchActivity.java
@@ -0,0 +1,13 @@
+package com.bt.devcon.android.intents;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WebSearchActivity extends Activity {
+	
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+    	setContentView(R.layout.search_layout);
+	}
+}
